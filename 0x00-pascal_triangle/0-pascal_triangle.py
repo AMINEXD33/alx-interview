3c5fca23a6622c9726a4b0007fc00775398abd4d
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-01 19:47:29 +0100

    pascal triangle approach

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,7 +4,7 @@ Module that defines a function to generate Pascal's triangle up to a given level
 """
 
 
-def try_get_elements_C1_C2(current_col: int, cached_row):
+def try_get_elements_c1_c2(current_col: int, cached_row):
     """
     Calculate the sum of two elements in the previous row of Pascal's triangle.
 
@@ -19,9 +19,9 @@ def try_get_elements_C1_C2(current_col: int, cached_row):
     position2 = current_col
     element1 = 0
     element2 = 0
-    if position1 >= 0 and position1 <= (len(cached_row) - 1):
+    if 0 <= position1 <= (len(cached_row) - 1):
         element1 = cached_row[position1]
-    if position2 >= 0 and position2 <= (len(cached_row) - 1):
+    if 0 <= position2 <= (len(cached_row) - 1):
         element2 = cached_row[position2]
     return element1 + element2
 
@@ -46,19 +46,19 @@ def pascal_triangle(n):
         row = [None] * (len(cached_row) + 1)  # allocate strictly the memory needed
         limit = len(cached_row)
         for col in range(0, limit + 1, 4):
-            element = try_get_elements_C1_C2(col, cached_row)
+            element = try_get_elements_c1_c2(col, cached_row)
             row[col] = element
 
             if col + 1 <= limit:
-                element = try_get_elements_C1_C2(col + 1, cached_row)
+                element = try_get_elements_c1_c2(col + 1, cached_row)
                 row[col + 1] = element
 
             if col + 2 <= limit:
-                element = try_get_elements_C1_C2(col + 2, cached_row)
+                element = try_get_elements_c1_c2(col + 2, cached_row)
                 row[col + 2] = element
 
             if col + 3 <= limit:
-                element = try_get_elements_C1_C2(col + 3, cached_row)
+                element = try_get_elements_c1_c2(col + 3, cached_row)
                 row[col + 3] = element
         cached_row = row
         row = []
