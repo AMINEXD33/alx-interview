8fa1d69e6836be5e5d9da9b5dc075e779bcf11b4
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-10 00:20:22 +0100

    all

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+"""a module that contains a function that solves the problem Lockboxes"""
+
+
 def canUnlockAll(boxes):
+    """
+    function that takes an array of boxes that may or
+    may not contain keys to other boxes
+    and determins if all boxes can be oppened
+    """
     count = 1
     cach = {}
     for box_index in range(len(boxes)):
