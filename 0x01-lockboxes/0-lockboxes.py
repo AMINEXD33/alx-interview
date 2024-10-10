30bc8bf1bd6bb384494df43492cb4bd40de617cd
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-10 01:06:37 +0100

    all

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,23 +2,35 @@
 """a module that contains a function that solves the problem Lockboxes"""
 
 
+def valid_box(boxes, jump_index):
+    """
+    check if a box is a valid box to check
+    """
+    if jump_index > len(boxes) - 1 or jump_index < 0:
+        return False
+    return True
+
+
 def canUnlockAll(boxes):
     """
     function that takes an array of boxes that may or
     may not contain keys to other boxes
-    and determins if all boxes can be oppened
+    and determines if all boxes can be opened.
     """
-    if not boxes:
-        return False
+    curr_box = 0
+    found_keys = [0]
+    already_opened = {0: True}
+    count = 1
 
-    n = len(boxes)
-    unlocked = [False]
-    unlocked[0] = True
-    keys = set(boxes[0])
+    while found_keys:
+        curr_box = found_keys.pop(0)
+        for key in boxes[curr_box]:
+            if key != curr_box and key not in already_opened:
+                found_keys.append(key)
+                already_opened[key] = True
+                count += 1
 
-    while keys:
-        new_key = keys.pop()
-        if new_key < n and not unlocked[new_key]:
-            unlocked[new_key] = True
-            keys.update(boxes[new_key])
-    return all(unlocked)
+        if count == len(boxes):
+            return True
+
+    return count == len(boxes)
