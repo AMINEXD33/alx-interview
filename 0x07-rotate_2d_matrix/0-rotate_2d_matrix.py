0f2745a09f52825594c78a25b887c320cbcf881a
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-21 00:37:33 +0100

    rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-"""this modul contains one function to rotate a n * n matrix"""
-import math
+"""
+this modul contains one function to rotate a n * n matrix
+"""
+
+
+def ceil(number):
+    """
+    a local implementation of the ceil function
+    since im not allowed to import any module -_-
+    """
+    if type(number) is float:
+        return int(number + 1)
+    else:
+        return number
 
 
 def rotate_2d_matrix(matrix) -> None:
@@ -8,18 +20,18 @@ def rotate_2d_matrix(matrix) -> None:
     a function that rotates a matrix of n * n type
     """
     # transpose matrix
-    matrix_lenght:int = len(matrix)
-    for i in range(math.ceil(matrix_lenght / 2)):
+    matrix_lenght: int = len(matrix)
+    for i in range(ceil(matrix_lenght / 2)):
         for j in range(i, matrix_lenght):
-            x:int = matrix[i][j]
-            y:int = matrix[j][i]
+            x: int = matrix[i][j]
+            y: int = matrix[j][i]
             matrix[j][i] = x
             matrix[i][j] = y
 
     # reverse rows
     for row in range(len(matrix)):
-        for element in range(math.ceil(len(matrix[row]) / 2) - 1):
-            first:int = matrix[row][element]
-            last:int = matrix[row][len(matrix[row]) - 1 - element]
+        for element in range(ceil(len(matrix[row]) / 2) - 1):
+            first: int = matrix[row][element]
+            last: int = matrix[row][len(matrix[row]) - 1 - element]
             matrix[row][element] = last
             matrix[row][len(matrix[row]) - 1 - element] = first
