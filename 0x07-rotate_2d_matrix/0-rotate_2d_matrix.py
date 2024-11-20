fe466e349f0e7f802fea22b43f041fbe2e70b963
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-21 00:29:38 +0100

    rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,18 +8,18 @@ def rotate_2d_matrix(matrix) -> None:
     a function that rotates a matrix of n * n type
     """
     # transpose matrix
-    matrix_lenght = len(matrix)
+    matrix_lenght:int = len(matrix)
     for i in range(math.ceil(matrix_lenght / 2)):
         for j in range(i, matrix_lenght):
-            x = matrix[i][j]
-            y = matrix[j][i]
+            x:int = matrix[i][j]
+            y:int = matrix[j][i]
             matrix[j][i] = x
             matrix[i][j] = y
 
     # reverse rows
     for row in range(len(matrix)):
         for element in range(math.ceil(len(matrix[row]) / 2) - 1):
-            first = matrix[row][element]
-            last = matrix[row][len(matrix[row]) - 1 - element]
+            first:int = matrix[row][element]
+            last:int = matrix[row][len(matrix[row]) - 1 - element]
             matrix[row][element] = last
             matrix[row][len(matrix[row]) - 1 - element] = first
