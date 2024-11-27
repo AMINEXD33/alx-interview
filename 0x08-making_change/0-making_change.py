629b2c29a8b17263e53313124aa1e203d7898022
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-27 16:20:56 +0100

    solved

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that has a function that solves the coin change problem"""
+import math
+
+
+def makeChange(coins, total):
+    """a function to calculate the minimum amount of coins
+        to get a total value
+    """
+    dp: list[int] = [math.inf] * (total + 1)
+    dp[0] = 0
+
+    for n in range(1, total + 1):
+        for coin in range(len(coins)):
+            if n - coins[coin] >= 0:
+                dp[n] = min(dp[n], 1 + dp[n - coins[coin]])
+
+    if dp[total] == math.inf:
+        return -1
+    return dp[total]
